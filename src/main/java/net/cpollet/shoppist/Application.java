36713d9063bb410da80c8c5477aef267ddf56fee
Christophe Pollet <cpollet@users.noreply.github.com>
2014-12-02 21:26:41 +0100

    First commit

---
+++ src/main/java/net/cpollet/shoppist/Application.java
@@ -0,0 +1,17 @@
+package net.cpollet.shoppist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends WebMvcAutoConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
